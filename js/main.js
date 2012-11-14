81a0d50eebf74c62513b8f4845fe844915d00033
xel51 <xel51@xel51-K53>
2012-11-14 04:09:40 +0000

    Add views for CV and Cover Letter and some menu changes

--- js/main.js
+++ js/main.js
@@ -6,8 +6,10 @@ function Resume() {
 }
 
 Resume.prototype.goToView = function( viewToGo ) {
-    $('.reveal').removeClass('current');
-    $('.mainView').removeClass('current');
+    $('.reveal').removeClass('current toLeft toRight');
+    $('.mainView').removeClass('current toLeft toRight');
+    $('.cv').removeClass('current');
+    $('.coverLetter').removeClass('current');
 
     if ( viewToGo == 'main' ){
         $('.mainView').addClass('current');
@@ -17,6 +19,18 @@ Resume.prototype.goToView = function( viewToGo ) {
     if ( viewToGo == 'awesomeSlides' ){
         $('.reveal').addClass('current');
         this.currentView = 'awesomeSlides';
+    }
+
+    if ( viewToGo == 'cv' ){
+        $( '.mainView, .reveal' ).addClass('toLeft')
+        $('.cv').addClass('current');
+        this.currentView = 'cv';
+    }
+
+    if ( viewToGo == 'coverLetter' ){
+        $( '.mainView, .reveal' ).addClass('toRight')
+        $('.coverLetter').addClass('current');
+        this.currentView = 'coverLetter';
     }
     console.log(this.currentView);
 }
